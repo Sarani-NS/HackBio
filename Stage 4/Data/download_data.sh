802ef160377886744f7b90e5b32c86126e4c99a6
Sarani <sarani.nativelsanterne@gmail.com>
2024-09-30 09:33:10 +0200

    Update and rename Stage 4/dummy file to Stage 4/Data/download_data.sh
    Run with: bash download_data.sh

---
+++ Stage 4/Data/download_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget "https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz" 
+
+wget "https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz"
+
+wget "https://zenodo.org/records/10886725/files/Reference.fasta" 
