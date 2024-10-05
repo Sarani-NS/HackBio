8cfa6aba190eea7d2c03bb2301d24f8f02fc561b
Sarani <sarani.nativelsanterne@gmail.com>
2024-10-05 23:07:03 +0200

    Delete Stage 4/Data directory

--- Stage 4/Data/download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget "https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz" 
-
-wget "https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz"
-
-wget "https://zenodo.org/records/10886725/files/Reference.fasta" 
